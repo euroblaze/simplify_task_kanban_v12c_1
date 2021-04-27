454c21e7330b89d97fc5c21cd094c35acafac752
bojan <bojan.ancev@inteligenta.io>
2021-04-27 11:34:33 +0200

    Simplify_task_kanban_v12c_1 module.

---
+++ __manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "simplify_task_kanban_v12c_1",
+
+    'summary': """
+        Hide Add a Column button on Project Task Kanban View.
+    """,
+
+    'description': """
+        Hide the button for adding a new column for users, 
+        allow access only to the users that have an option to activate the developer/debug mode.
+    """,
+
+    'author': "Simplify ERP",
+    'website': "https://simplify-erp.com/",
+    'category': 'Uncategorized',
+    'version': '1.0',
+    'depends': ['base', 'project'],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
